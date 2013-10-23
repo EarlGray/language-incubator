7f36b3f105725726e1eaa21648f11f9bf38f94c8
Dm. Sirenko <dmytrish@gmail.com>
2013-10-23 19:29:51 +0300

    HasmParse.hs compilable draft:
    e.g. ".l1: mov %eax, %ebx\n"

--- x86/src/HasmParse.hs
+++ x86/src/HasmParse.hs
@@ -1,26 +1,34 @@
 module HasmParse where
 
+import Data.List (intercalate)
+
+import Control.Applicative ((<$>), (<*), liftA2)
+import Control.Monad (when)
+
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
 import HasmImports
 import HasmTypes
+import X86CPU
 
-import Control.Applicative ((<$>))
-import Control.Monad (when)
-import Text.Parsec
+type ParseResult = [(HasmStatement, SourcePos)]
 
-hasmParseFile :: FilePath -> IO (Either ParseError [HasmStatement])
+hasmParseFile :: FilePath -> IO (Either ParseError ParseResult)
 hasmParseFile fname = hasmParseWithSource fname <$> readFile fname
 
-hasmParseWithSource :: FilePath -> String -> Either ParseError [HasmStatement]
+hasmParseWithSource :: FilePath -> String -> Either ParseError ParseResult
 hasmParseWithSource fname ss = parse asmfile fname ss
 
-asmfile :: Parser [HasmStatement]
-asmfile = many asmstmt
+asmfile :: Parser ParseResult
+asmfile = many $ do
+  optional asmspaces
+  pos <- getPosition
+  stmt <- asmstmt
+  return (stmt, pos)
 
 asmstmt :: Parser HasmStatement
-asmstmt = try asmdir <|> try asmlabel <|> asmop
+asmstmt = try asmdir <|> try asmlabel <|> asmop <?> "assembly statement"
 
 dirchar = letter <|> char '_'
 idschar = letter <|> oneOf "_"
@@ -29,7 +37,7 @@ idchar = idschar <|> digit <|> oneOf ".$"
 endstmt = oneOf ";\n"
 asmspaces = many1 (oneOf " \t")
 
-cons a b = lift2 (:) a b
+cons a b = liftA2 (:) a b
 opchar = letter <|> digit
 opcode = cons letter (many1 opchar)
 regname = cons letter (many1 opchar)
@@ -37,22 +45,27 @@ regname = cons letter (many1 opchar)
 asmdir = do
   char '.'
   dir <- many1 dirchar
+  dirargs <- sepBy (many1 $ noneOf "\n,") (char ',') <* endstmt
+  case readDirective dir dirargs of
+    Right d -> return $ HasmStDirective d
+    Left e -> parserFail e
 
-asmlabel = cons idschar (many1 idchar) <* char ':' <?> "label"
+asmlabel = HasmStLabel <$> cons idschar (many1 idchar) <* char ':' <?> "label"
 
 asmop = do
   op <- opcode
-  when (not (isOpcode op)) $ parserFail ("Unknown opcode: " ++ op)
-
   asmspaces
-  opnds <- sepBy (char ',' >> optional (oneOf " \t")) asmopnd
-  optional asmcomment
+  opnds <- sepBy asmopnd (char ',' >> optional (oneOf " \t"))
+  --optional asmcomment
   endstmt
-  return $ mkOp op opnds
+  case readOperation op opnds of
+    Left e -> parserFail e
+    Right o -> return $ HasmStInstr [] o
 
 asmopnd :: Parser OpOperand
-asmopnd = try asmregister <|> try asmaddr <|> try asmmem <?> "opcode operand"
+asmopnd = try (OpndReg <$> asmregister) {-<|> try asmaddr <|> try asmmem-} <?> "opcode operand"
 
+{-
 asmmem = do
   mbDspl <- optionMaybe asmdispl
   case mbDspl of
@@ -61,12 +74,23 @@ asmmem = do
 asmdispl = 
 
 asmaddr = 
+-}
 
-asmregister :: NamedRegister a => String -> a
 asmregister = do
   char '%'
   reg <- regname
   case regByName reg of
-    Just r -> r
+    Just r -> return r
     _ -> parserFail $ "Not a register name: " ++ reg
   
+
+readDirective :: String -> [String] -> Either String Directive
+readDirective dir args =
+  case dir of
+    _ -> Left $ "Unknown directive: " ++ dir ++ intercalate "," args
+
+readOperation :: String -> [OpOperand] -> Either String Operation
+readOperation opname args =
+  case opname of
+    "mov" -> Right $ Operation OpMov args
+    _ -> Left $ "Unknown operation: " ++ opname
