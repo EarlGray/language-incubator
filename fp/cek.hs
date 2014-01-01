a90cf02131fd53d8b569486ad1fec314fe6b2905
EarlGray <dmytrish@gmail.com>
2014-01-01 13:39:49 +0200

    Working CEK

--- fp/cek.hs
+++ fp/cek.hs
@@ -27,6 +27,7 @@ data Closure = Clo Lam Env
 data Kont
     = KStart
     | KEmpty
+    | KEnd
     | KHoleFun (Exp, Env) Kont
     | KHoleArg Val Kont
   deriving (Read, Show)
@@ -58,17 +59,17 @@ step (MVal (VLam lam), e,  k)          = (MVal (VClo (Clo lam e)), e, k)
 step (MVal w,      e1, KHoleFun (m, e2) k)    = (m, e2, KHoleArg w k)
 step (MVal w,      e1, KHoleArg (VClo (Clo (x :=> m) e2)) k)
     = (m, extendEnv (x, w) e2, k)
---step (w,     e, k) = (w, e, KEmpty)
+step (w,     e, k) = (w, e, KEnd)
 
 run :: State -> State
-run (exp, env, KEmpty) = (exp, env, KEmpty)
+run (exp, env, KEnd) = (exp, env, KEnd)
 run (exp, env, KStart) = run $ step (exp, env, KEmpty)
 run (exp, env, k)      = run $ step (exp, env, k)
 
 evaluate :: Program -> Exp
 evaluate exp = 
     case run (exp, emptyEnv, KStart) of
-      (result, _, KEmpty) -> result
+      (result, _, KEnd) -> result
       st -> error $ "Program exited with invalid state " ++ show st
 
 {-
@@ -77,19 +78,20 @@ evaluate exp =
 lam = MVal . VLam
 int = MVal . VInt
 ref = MRef
+(<.>) = MAp
+infixl 9 <.> 
 
 test_id = lam ("x":=>ref "x")
 test_int = int 42
-test1 = MAp test_id test_int
-test2 = MAp (MAp (lam ("x" :=> (lam ("y" :=> (ref "x"))))) (int 1)) (int 2)
+test1 = test_id <.> test_int
+test2 = ((lam("x":=>(lam("y":=>ref "x")))) <.> (int 1)) <.> (int 2)
 
 p0 = lam("f":=>lam("x":=>ref "x"))
-p1 = lam("f":=>lam("x":=>(MAp (ref "f") (ref "x"))))
+p1 = lam("f":=>lam("x":=>(ref "f" <.> ref "x")))
 psucc = lam("n":=>(
           lam ("f" :=>(
             lam ("x" :=>(
-              MAp (ref "f")
-                  (MAp (MAp (ref "n") (ref "f")) (ref "x"))))))))
+              ref "f" <.> (ref "n" <.> ref "f" <.> ref "x")))))))
 p2 = MAp psucc p1
 
 
@@ -109,10 +111,14 @@ pretty (MVal v) =
     case v of
       VInt n -> show n
       VLam (x :=> m) -> "λ" ++ x ++ "." ++ pretty m ++ ""
-      VClo (Clo (x :=> m) _) -> "(Λ" ++ x ++ "." ++ pretty m ++ ")"
+      VClo (Clo (x :=> m) env) -> "(Λ" ++ x ++ "." ++ pretty m ++ ") {" ++ prettyEnv env ++ "}"
 
-putPretty :: Exp -> IO ()
 putPretty = putStrLn . pretty
+
+prettyEnv :: Env -> String
+prettyEnv = concat . map prettyBinding . M.toList
+    where prettyBinding (var, val) = var ++ " -> " ++ pretty (MVal val) ++ "\n"
+putEnv = putStrLn . prettyEnv
 
 main = do
     exp <- readLn :: IO Program
