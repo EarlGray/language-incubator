262524b66264645911bd2b22d8da2e3d2963264d
EarlGray <dmytrish@gmail.com>
2014-06-09 00:07:46 +0300

    HsKldscp: halfway on Chapter3

--- llvm/kldscp/kldscp.hs
+++ llvm/kldscp/kldscp.hs
@@ -2,9 +2,11 @@
  -  See: http://www.stephendiehl.com/llvm/
  -}
 
-
-import Control.Applicative ((<$>), (<*), (*>))
+import Data.Word
+import qualified Data.Map as M
+import Control.Applicative (Applicative(..), (<$>), (<*), (*>))
 import Control.Monad.Trans
+import Control.Monad.State (gets, modify, MonadState(..), execState)
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
@@ -14,6 +16,16 @@ import qualified Text.Parsec.Expr as Ex
 import qualified Text.Parsec.Token as Tok
 
 import System.Console.Haskeline
+
+import LLVM.General
+import qualified LLVM.General.AST as AST
+import LLVM.General.AST (functionDefaults, moduleDefinitions,
+    defaultModule, Global(..), BasicBlock(..), Module(..),
+    {-Function(..),-} Definition(..), Parameter(..), Terminator(..))
+import LLVM.General.AST.Type (Type(..), FloatingPointFormat(..))
+import LLVM.General.AST.Name
+import LLVM.General.AST.Operand (Operand(..))
+import LLVM.General.AST.Instruction (Instruction(..))
 
 {- Lexer -}
 
@@ -36,15 +48,15 @@ identifier = Tok.identifier lexer
 reserved   = Tok.reserved lexer
 reservedOp = Tok.reservedOp lexer
 
-type Name = String
+type KName = String
 
 data Expr
     = Float Double
     | BinOp Op Expr Expr
     | Var String
-    | Call Name [Expr]
-    | Function Name [Expr] Expr
-    | Extern Name [Expr]
+    | KCall KName [Expr]
+    | KFunction KName [Expr] Expr
+    | Extern KName [Expr]
     deriving (Eq, Ord, Show)
 
 data Op = Plus | Minus | Times | Divide deriving (Eq, Ord, Show)
@@ -59,19 +71,19 @@ table = [[binary "*" Times Ex.AssocLeft,
          [binary "+" Plus Ex.AssocLeft,
           binary "-" Minus Ex.AssocLeft]]
 
-int, floating, expr, function, extern, call, factor :: Parser Expr
+int, floating, expr, defp, extern, call, factor :: Parser Expr
 
 int = Float <$> fromInteger <$> integer
 floating = Float <$> float
 expr = Ex.buildExpressionParser table factor
 variable = Var <$> identifier
 
-function = do
+defp = do
     reserved "def"
     name <- identifier
     args <- parens $ many variable
     body <- expr
-    return $ Function name args body
+    return $ KFunction name args body
 
 extern = do
     reserved "extern"
@@ -82,18 +94,18 @@ extern = do
 call = do
     name <- identifier
     args <- parens $ commaSep expr
-    return $ Call name args
+    return $ KCall name args
 
 factor = try floating
      <|> try int
      <|> try extern
-     <|> try function
+     <|> try defp
      <|> try call
      <|> variable
      <|> parens expr
 
 defn = try extern
-    <|> try function
+    <|> try defp
     <|> expr
 
 contents p = Tok.whiteSpace lexer *> p <* eof
@@ -123,4 +135,86 @@ parserMain = runInputT defaultSettings loop
         Nothing -> outputStrLn "bye."
         Just input -> liftIO (parserREPL input) >> loop
 
+
+{- Code generator -}
+type Names = M.Map String Int
+
+double :: Type
+double = FloatingPointType 64 IEEE
+
+type SymbolTable = [(String, Operand)]
+
+data CodegenState
+    = CodegenState {
+        currentBlock    :: Name,
+        blocks          :: M.Map Name BlockState,
+        symtab          :: SymbolTable,
+        blockCount      :: Int,
+        count           :: Word,
+        names           :: Names
+    } deriving Show
+
+data BlockState
+    = BlockState {
+        idx   :: Int,
+        stack :: [AST.Named Instruction],
+        term  :: Maybe (AST.Named Terminator)
+    } deriving Show
+
+newtype Codegen a = Codegen { runCodegen :: State CodegenState a }
+            deriving (Functor, Applicative, Monad, MonadState CodegenState)
+
+newtype LLVM a = LLVM { unLLVM :: State AST.Module a }
+            deriving (Functor, Applicative, Monad, MonadState AST.Module)
+
+runLLVM :: AST.Module -> LLVM a -> AST.Module
+runLLVM = flip (execState . unLLVM)
+
+emptyModule :: String -> AST.Module
+emptyModule label = defaultModule { moduleName = label }
+
+addDefn :: Definition -> LLVM ()
+addDefn d = do
+    defs <- gets moduleDefinitions
+    modify $ \s -> s { moduleDefinitions = defs ++ [d] }
+
+define :: Type -> String -> [(Type, Name)] -> [BasicBlock] -> LLVM ()
+define retty label argtys body =
+    addDefn $ GlobalDefinition $ functionDefaults {
+        name = Name Label,
+        parameters = ([Parameter ty nm [] | (ty, nm) <- argtys], False),
+        returnType = retty,
+        basicBlocks = body
+    }
+
+external :: Type -> String -> [(Type, Name)] -> LLVM ()
+external retty label argtys =
+    addDefn $ GlobalDefinition $ functionDefaults {
+        name = Name Label,
+        parameters = ([Parameter ty nm [] | (ty, nm) <- argtys], False),
+        returnType = retty,
+        basicBlocks = []
+    }
+
+entry :: Codegen Name
+entry = gets currentBlock
+
+addBlock :: String -> Codegen Name
+addBlock bname = do
+    bis <- gets blocks
+    ix <- gets blockCount
+    nms <- gets names
+
+    let new = emptyBlock ix
+        (qname, supply) = uniqueName bname nms
+
+    modify $ \s ->
+      s {
+        blocks = M.insert (Name qname) new bis,
+        blockCount = ix + 1,
+        names = supply }
+    return $ Name qname
+
 main = parserMain
+
+
