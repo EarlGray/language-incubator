b01e272fe36d7c50e514aaf7211f297e55b8e8a9
Dmytro S <dmytrish@gmail.com>
2014-08-16 20:22:20 +0300

    Added factorial_s example in HasmTest.hs

--- x86/src/HasmTest.hs
+++ x86/src/HasmTest.hs
@@ -8,22 +8,49 @@ import HasmPretty
 {-
  -  Test Examples
  -}
-movinstr = HasmStInstr [] . Operation OpMov 
-intinstr = HasmStInstr [] . Operation OpInt
+regl = OpndReg . RegL
+imml = OpndImm . ImmL
+immb = OpndImm . ImmB
+label l = OpndRM noSIB (DisplLabel l)
+
+instr i = HasmStInstr [] . Operation i
+
+regdspl b d = OpndRM (SIB 1 Nothing (Just b)) d
+
 linux_null_s = [
-  HasmStLabel "_start",                           -- _start:
-  movinstr [OpndImm (ImmL 0x0), OpndReg (RegL RegEAX)],  --    movl $1, %eax
-  movinstr [OpndImm (ImmL 0x1), OpndReg (RegL RegEBX)],  --    movl $0, %ebx
-  intinstr [OpndImm (ImmB 0x80)]               ]  --    int 0x80
+  HasmStLabel "_start",                 -- _start:
+  instr OpMov [imml 0x0, regl RegEAX],  --    movl $1, %eax
+  instr OpMov [imml 0x1, regl RegEBX],  --    movl $0, %ebx
+  instr OpInt [immb 0x80] ]             --    int 0x80
 
 label_mov_s = [
-  movinstr [OpndRM (SIB 1 Nothing Nothing) (DisplLabel "x"),
-                                   OpndReg (RegL RegEAX)]] 
+  instr OpMov [label "x", regl RegEAX]]
 
 loop_jmp_s = [
   HasmStLabel "_start",
   HasmStLabel "loop_start",
-  HasmStInstr [] $ Operation OpJmp [OpndRM noSIB (DisplLabel "loop_start")] ]
+  instr OpJmp [label "loop_start"] ]
 
+{-
+factorial_s = [
+  instr OpMov [regdspl RegESP (Displ8 4), regl RegECX],
+  instr OpCmp [imml 1, regl RegECX],
+  instr OpJE  [label "lbl1"],
+  instr OpMov [imml 1, regl RegEAX],
+  instr OpMov [imml 1, regl RegEDX],
+  HasmStLabel "lbl2",
+  instr OpIMul [regl RegEDX, regl RegEAX],
+  instr OpAdd [imml 1, regl RegEDX],
+  instr OpCmp [regl RegECX, regl RegEDX],
+  instr OpJne [label "lbl2"],
+  instr OpRet [],
+  HasmStLabel "lbl1",
+  instr OpMov [imml 1, regl RegEAX],
+  instr OpRet [] ]
+-}
 
 fromRight = either (error "fromRight $ Left ...") id
+
+assembleStmts :: [HasmStatement] -> [(HasmStatement, SrcPos, [Word8])]
+assembleStmts = fromRight . assembleFromZero . withTestSrc
+
