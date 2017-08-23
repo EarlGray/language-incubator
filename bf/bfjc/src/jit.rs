35bec821fa7fde5773763b8104814a63f4f04282
Dmytro S <dmytrish@gmail.com>
2017-08-23 22:39:53 +0200

    bf/bfjc: jit.rs, bin/bfjc3

---
+++ bf/bfjc/src/jit.rs
@@ -0,0 +1,82 @@
+extern crate libc;
+
+use std::mem;
+use std::ops::{Index, IndexMut};
+
+
+/*
+ *  Pages
+ */
+
+
+pub struct Pages(pub usize);
+
+impl Pages {
+    const SIZE: usize = 4096;
+
+    pub fn byte_size(&self) -> usize {
+        self.0 * Pages::SIZE
+    }
+}
+
+
+/*
+ *  JITting
+ */
+pub struct Memory {
+    contents: *mut u8,
+    size: usize,
+    emit_pos: usize
+}
+
+impl Index<usize> for Memory {
+    type Output = u8;
+
+    fn index(&self, _index: usize) -> &u8 {
+        unsafe { &*self.contents.offset(_index as isize) }
+    }
+}
+
+impl IndexMut<usize> for Memory {
+    fn index_mut(&mut self, _index: usize) -> &mut u8 {
+        let size = self.size;
+        if _index >= size {
+            panic!("More than {} bytes to emit", size);
+        }
+
+        unsafe { &mut *self.contents.offset(_index as isize) }
+    }
+}
+
+impl Memory {
+    pub fn new(pages: Pages) -> Memory {
+        let contents: *mut u8;
+        let size = pages.byte_size();
+        unsafe {
+            let mut _contents: *mut libc::c_void = mem::uninitialized();
+
+            libc::posix_memalign(&mut _contents, Pages::SIZE, size);
+            let flags = libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC;
+            libc::mprotect(_contents, size, flags);
+
+            // libc::memset(_contents, 0xc3, size);  // RET
+            contents = mem::transmute(_contents);
+        }
+
+        Memory { contents: contents, emit_pos: 0, size: size }
+    }
+
+    pub fn emit(&mut self, code: &[u8]) {
+        for b in code.iter() {
+            let pos = self.emit_pos;
+            self[pos] = *b;
+            self.emit_pos += 1;
+        }
+    }
+
+    pub fn get_entry(&self) -> (fn() -> i64) {
+        unsafe { mem::transmute(self.contents) }
+    }
+}
+
+
