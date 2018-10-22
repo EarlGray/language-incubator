9cba3d3457e76416e98ebc028b7da5b9b54f88cf
Dmytro S <me@dmytrish.net>
2018-10-22 22:25:14 +0200

    push

--- x86/hasm2/hasm.hs
+++ x86/hasm2/hasm.hs
@@ -22,7 +22,7 @@ immValue (Imm8 i8) = int i8
 immValue (Imm16 i16) = int i16
 immValue (Imm32 i32) = i32
 
--- | 
+-- |
 -- | Registers and memory references
 -- |
 
@@ -60,7 +60,7 @@ displacement NoDispl = 0
 displacement (Displ8 d) = int d
 displacement (Displ32 d) = d
 
--- | 
+-- |
 -- | Instructions
 -- |
 
@@ -147,9 +147,9 @@ instance PrettyPrintable Instruction where
         ]
 
 
--- | 
+-- |
 -- | Encoding of instructions
--- | 
+-- |
 
 type RegMask = Word8
 type ImmMask = Word32
@@ -187,10 +187,51 @@ encodePrefix pre = maybe err Right $ lookup pre prefixes
    where err = Left $ "prefix is unknown: " ++ pre
 
 
+type InstructionSyntax = (String, [OpArgument -> Bool])
+
 type EncodingError = String
+
+data CodeByArgSize
+    = ForAnySize [Word8]
+    | ForSize8 [Word8]
+    | ForSize32 [Word8]
+    | ForSize32Reg [Word8]              -- [op + index(%reg)]
+    | ForSize8and32 [Word8] [Word8]
+
 data Encoder = Encoder {
-    getEncoder :: [OpArgument] -> Either EncodingError [Word8]
+    getEncoder :: [OpArgument] -> Either EncodingError [Word8],
+    getInstruction :: CodeByArgSize
 }
+
+modRMmd0  r rm  = 0x00 .|. (r `shiftL` 3) .|. (regIndex rm)
+modRMmd8  r rm  = 0x40 .|. (r `shiftL` 3) .|. (regIndex rm)
+modRMmd32 r rm  = 0x80 .|. (r `shiftL` 3) .|. (regIndex rm)
+modRMreg  r rm  = 0xc0 .|. (r `shiftL` 3) .|. (regIndex rm)
+
+encodeMemRef :: Word8 -> (Displacement, Maybe NamedReg32, Maybe (SIBScale, NamedReg32))
+                -> Either EncodingError [Word8]
+encodeMemRef reg (d, Nothing, Nothing) =
+    -- `displ(%ebp)` means `displ`:
+    let modRM = modRMmd0 reg (Reg32 EBP) in
+    Right $ [modRM] ++ encodeDisplacement d
+encodeMemRef reg (d, (Just base), Nothing) =
+    -- because %esp in modrm means "use sib":
+    let espSIB = if base == ESP then [0x24] else []
+    in case (base, d) of
+      (EBP, NoDispl) ->
+        -- because (%ebp) must be 0(%ebp):
+        encodeMemRef reg (Displ8 0, Just base, Nothing)
+      (_, NoDispl) ->
+        Right (modRMmd0 reg (Reg32 base) : espSIB)
+      (_, Displ8 d) ->
+        Right ([modRMmd8 reg (Reg32 base)] ++ espSIB ++ [d])
+      (_, Displ32 d) ->
+        Right ([modRMmd32 reg (Reg32 base)] ++ espSIB ++ dbytes)
+          where dbytes = BL.unpack $ Bin.runPut $ Bin.putWord32le d
+encodeMemRef reg (d, Nothing, Just (index, sc)) =
+    Left "TODO"
+encodeMemRef reg (d, Just base, Just (index, sc)) =
+    Left "TODO"
 
 encodeImm :: Imm -> [Word8]
 encodeImm imm = BL.unpack $ Bin.runPut $ case imm of
@@ -204,63 +245,40 @@ encodeDisplacement (Displ32 d) =
     BL.unpack $ Bin.runPut $ Bin.putWord32le d
 
 
-modRMmd0  r rm  = 0x00 .|. (regIndex r `shiftL` 3) .|. (regIndex rm)
-modRMmd8  r rm  = 0x40 .|. (regIndex r `shiftL` 3) .|. (regIndex rm)
-modRMmd32 r rm  = 0x80 .|. (regIndex r `shiftL` 3) .|. (regIndex rm)
-modRMreg  r rm  = 0xc0 .|. (regIndex r `shiftL` 3) .|. (regIndex rm)
 
-encodeNP _ = Right []
+encNP _ = Right []
 
-encodeI8 [ArgImm (Imm8 val)] = Right [val]
-encodeI8 args = Left $ "expected imm8, got: " ++ show args
+encI8 [ArgImm (Imm8 val)] = Right [val]
+encI8 args = Left $ "expected imm8, got: " ++ show args
 
 -- First Displacement?
-encodeFD [ArgMem (MemRef d _ _), _] = Right $ encodeDisplacement d
-encodeFD args = Left $ "Expected `op displ(_), _`, got: " ++ show args
+encFD [ArgMem (MemRef d _ _), _] = Right $ encodeDisplacement d
+encFD args = Left $ "Expected `op displ(_), _`, got: " ++ show args
 
-encodeTD [_, ArgMem (MemRef d _ _)] = Right $ encodeDisplacement d
-encodeTD args = Left $ "Expected `op _, displ(_)`, got: " ++ show args
+encTD [_, ArgMem (MemRef d _ _)] = Right $ encodeDisplacement d
+encTD args = Left $ "Expected `op _, displ(_)`, got: " ++ show args
 
 
-encodeMR [ArgReg reg, ArgReg rm] = 
-    Right [modRMreg reg rm]
-encodeMR [ArgReg reg, ArgMem (MemRef d Nothing Nothing)] =
-    -- `displ(%ebp)` means `displ`:
-    Right $ [modRMmd0 reg (Reg32 EBP)] ++ encodeDisplacement d
-encodeMR [ArgReg reg, ArgMem (MemRef d (Just base) Nothing)] =
-    -- because %esp in modrm means "use sib":
-    let espSIB = if base == ESP then [0x24] else []
-    in case (base, d) of
-      (EBP, NoDispl) ->
-        -- because (%ebp) must be 0(%ebp):
-        encodeMR [ArgReg reg, ArgMem (MemRef (Displ8 0) (Just base) Nothing)]
-      (_, NoDispl) ->
-        Right (modRMmd0 reg (Reg32 base) : espSIB)
-      (_, Displ8 d) ->
-        Right ([modRMmd8 reg (Reg32 base)] ++ espSIB ++ [d])
-      (_, Displ32 d) ->
-        Right ([modRMmd32 reg (Reg32 base)] ++ espSIB ++ dbytes)
-          where dbytes = BL.unpack $ Bin.runPut $ Bin.putWord32le d
-encodeMR [ArgReg reg, ArgMem (MemRef d Nothing (Just (index, sc)))] =
-    Left "TODO"
-encodeMR [ArgReg reg, ArgMem (MemRef d (Just base) (Just (index, sc)))] =
-    Left "TODO"
-encodeMR args = Left $ "expected `op %reg, r/m`, got: " ++ show args
+encMR [ArgReg reg, ArgReg rm] =
+    Right [modRMreg (regIndex reg) rm]
+encMR [ArgReg reg, ArgMem (MemRef d base index)] =
+    encodeMemRef (regIndex reg) (d, base, index)
+encMR args = Left $ "expected `op %reg, r/m`, got: " ++ show args
 
-encodeRM [arg1@(ArgMem _), arg2@(ArgReg _)] = encodeMR [arg2, arg1]
-encodeRM args = Left $ "expected `op r/m, %reg`, got: " ++ show args
+encM stub [ArgMem (MemRef displ base index)] =
+    encodeMemRef stub (displ, base, index)
+encM _ args = Left $ "expected `op r/m`, got: " ++ show args
 
-encoderMI stub [ArgReg r, ArgImm imm] =
-    Right (modRMreg (Reg32 (toEnum stub)) r : encodeImm imm)
-encoderMI stub args = 
+encRM [arg1@(ArgMem _), arg2@(ArgReg _)] = encMR [arg2, arg1]
+encRM args = Left $ "expected `op r/m, %reg`, got: " ++ show args
+
+encMI stub [ArgReg r, ArgImm imm] =
+    Right (modRMreg stub r : encodeImm imm)
+encMI stub args =
     Left $ "expected `op/" ++ show stub ++ " %reg, $imm`, got: " ++ show args
 
-data CodeByArgSize
-    = ForAnySize [Word8]
-    | ForSize8 [Word8]
-    | ForSize8and32 [Word8] [Word8]
-
-type InstructionSyntax = (String, [OpArgument -> Bool])
+encO [ArgReg r] = Right []
+encO args = Left $ "expected %reg, got: " ++ show args
 
 --- operand predicates:
 opLit val (ArgImm imm) = val == immValue imm
@@ -278,12 +296,6 @@ opReg _ = False
 opRegA (ArgReg r) = regIndex r == 0
 opRegA _ = False
 
-opRegFor index (ArgReg r) = index == regIndex r
-opRegFor _ _ = False
-
-opRegExcept index (ArgReg r) = index /= regIndex r
-opRegExcept _ _ = False
-
 opMoff (ArgMem (MemRef _ Nothing Nothing)) = True
 opMoff _ = False
 
@@ -295,73 +307,69 @@ opRM arg = opReg arg || opMem arg
 
 --- instruction encodings:
 
-instructions :: [(InstructionSyntax, CodeByArgSize, Encoder)]
+instructions :: [(InstructionSyntax, Encoder)]
 instructions = [
-  (("int", [opLit 3]),      ForSize8 [0xCC],             Encoder encodeNP),
-  (("int", [opImm8]),       ForSize8 [0xCD],             Encoder encodeI8),
-  (("into", []),            ForAnySize [0xCE],           Encoder encodeNP),
+  (("int", [opLit 3]),      Encoder encNP (ForSize8 [0xCC])),
+  (("int", [opImm8]),       Encoder encI8 (ForSize8 [0xCD])),
+  (("into", []),            Encoder encNP (ForAnySize [0xCE])),
 
   --(("mov", [opImm, opReg]),  ForSize8and32 [0xb0] [0xb8],    Encoder encoderOI),
-  (("mov", [opReg, opImm]),  ForSize8and32 [0xc6] [0xc7],    Encoder (encoderMI 0)),
-  (("mov", [opMoff, opRegA]), ForSize8and32 [0xa0] [0xa1],   Encoder encodeFD),
-  (("mov", [opRegA, opMoff]), ForSize8and32 [0xa2] [0xa3],   Encoder encodeTD),
-  (("mov", [opReg, opRM]),  ForSize8and32 [0x88] [0x89],  Encoder encodeMR),
-  (("mov", [opRM, opReg]),  ForSize8and32 [0x8a] [0x8b],  Encoder encodeRM)
+  (("mov", [opReg, opImm]),     Encoder (encMI 0) (ForSize8and32 [0xc6] [0xc7])),
+  (("mov", [opMoff, opRegA]),   Encoder encFD   (ForSize8and32 [0xa0] [0xa1])),
+  (("mov", [opRegA, opMoff]),   Encoder encTD   (ForSize8and32 [0xa2] [0xa3])),
+  (("mov", [opReg, opRM]),      Encoder encMR   (ForSize8and32 [0x88] [0x89])),
+  (("mov", [opRM, opReg]),      Encoder encRM   (ForSize8and32 [0x8a] [0x8b])),
   -- (("mov", [opSReg, opReg16]), ForSize16 [0x8c],     EncoderRM),
   -- (("mov", [opReg16, opSReg]), ForSize16 [0x8e],     EncoderRM),
+
+  (("push", [opReg]),        Encoder encO   (ForSize32Reg [0x50])),
+  (("push", [opRM]),         Encoder (encM 6) (ForSize32 [0xff]))
   ]
 
-{-
-  (("xor", [opA8,  OpImm S8]), mode_all,  [0x34],           EncodeI),
-  (("xor", [opADef, OpImm SDef]), mode_all, [0x35],         EncodeI),
-  (("xor", [opRM8, OpImm S8]),  mode_all,   [0x80],         EncodeMI 6),
-  (("xor", [opRMa, OpImm SAlt]), mode_all,  [preAddrsz, 0x81],  EncodeMI 6),
-  (("xor", [opRM,  OpImm SDef]), mode_all,  [0x81],         EncodeMI 6),
-  (("xor", [opRM8, opR8]),      mode_all,   [0x30],         EncodeMR),
-  (("xor", [opRM,  opR]),        mode_all,   [0x31],         EncodeMR)
-  -}
 
-
--- | 
+-- |
 -- | Assembling
 -- |
 
-findEncoders :: (String, [OpArgument]) -> Either EncodingError (CodeByArgSize, Encoder)
+findEncoders :: (String, [OpArgument]) -> Either EncodingError Encoder
 findEncoders (name, args) = maybe no_enc found $ L.find by_name_args instructions
   where
     no_enc = Left $ "could not find encoder for " ++ name ++ " " ++ show args
-    by_name_args ((instr, argspec), _, _) = 
+    by_name_args ((instr, argspec), _) =
         name == instr && all id (zipWith ($) argspec args)
-    found (_, op_enc, arg_enc) = Right (op_enc, arg_enc)
+    found (_, enc) = Right enc
 
-encodeOp codeforsize args = 
+encodeOp codeforsize args =
     case codeforsize of
         ForAnySize bytes ->
             Right bytes
-        ForSize8 bytes | all isSize8 args -> 
+        ForSize8 bytes | all isSize8 args ->
             Right bytes
         ForSize8and32 bytes _ | all isSize8 args ->
             Right bytes
         ForSize8and32 _ bytes | all isSize32 args ->
             Right bytes
-        _ -> Left $ "no encoder for " ++ show args
+        ForSize32Reg [b] ->
+            let [ArgReg r] = args
+            in Right [b + regIndex r]
+        _ -> Left $ "no encoding for " ++ show args
 
 encodeInstruction :: Instruction -> Either EncodingError [Word8]
 encodeInstruction (Op prefs name args) = do
-    (op_enc, Encoder arg_enc) <- findEncoders (name, args)
-    let pref_bits = concat <$> sequenceA (map encodePrefix prefs) :: Either EncodingError [Word8]
+    Encoder arg_enc op_enc <- findEncoders (name, args)
+    let pref_bits = concat <$> sequenceA (map encodePrefix prefs)
     let op_bits = encodeOp op_enc args
     let args_bits = arg_enc args :: Either EncodingError [Word8]
     concat <$> sequenceA [pref_bits, op_bits, args_bits]
-    
+
 
 {-
  -  Tests
  -}
 testCodegen tests = mapMaybe test tests
   where
-    test (op, expected) = 
-      let got = encodeInstruction op 
+    test (op, expected) =
+      let got = encodeInstruction op
       in if got == expected
         then Nothing
         else Just $ (op, expected, got)
@@ -370,33 +378,45 @@ imm n = ArgImm (Imm32 n)
 
 eax = ArgReg $ Reg32 EAX
 ecx = ArgReg $ Reg32 ECX
+esp = ArgReg $ Reg32 ESP
 
-moff d          = ArgMem $ MemRef (Displ32 d) Nothing Nothing 
-moff8 d         = ArgMem $ MemRef (Displ8 d)  Nothing Nothing 
+moff d          = ArgMem $ MemRef (Displ32 d) Nothing Nothing
+moff8 d         = ArgMem $ MemRef (Displ8 d)  Nothing Nothing
 ref1 r          = ArgMem $ MemRef NoDispl     (Just r) Nothing
-ref8 d r        = ArgMem $ MemRef (Displ8 d) (Just r) Nothing 
-refd d r        = ArgMem $ MemRef (Displ32 d) (Just r) Nothing 
+ref8 d r        = ArgMem $ MemRef (Displ8 d) (Just r) Nothing
+refd d r        = ArgMem $ MemRef (Displ32 d) (Just r) Nothing
 ref3 (b, i, s)  = ArgMem $ MemRef NoDispl     (Just b) (Just (s, i))
 ref d (b, i, s) = ArgMem $ MemRef (Displ32 d) (Just b) (Just (s, i))
 
+mov = Op [] "mov"
 movTests = [
-  (Op [] "mov" [eax, eax],                     Right [0x89, 0xc0]),
-  (Op [] "mov" [ref1 EAX, eax],                Right [0x8b, 0x00]),
-  (Op [] "mov" [ref1 ESP, eax],                Right [0x8b, 0x04, 0x24]),
-  (Op [] "mov" [ref1 EBP, eax],                Right [0x8b, 0x45, 0x00]),
-  (Op [] "mov" [ref8 4(ECX), eax],             Right [0x8b, 0x41, 0x04]),
-  (Op [] "mov" [ref8 4(EBP), eax],             Right [0x8b, 0x45, 0x04]),
-  (Op [] "mov" [ref8 4(ESP), eax],             Right [0x8b, 0x44, 0x24, 0x04]),
-  (Op [] "mov" [moff 0xdeadbeef, eax],         Right [0xa1, 0xef, 0xbe, 0xad, 0xde]),
-  (Op [] "mov" [moff 0xdeadbeef, ecx],         Right [0x8b, 0x0d, 0xef, 0xbe, 0xad, 0xde])
+  (mov [eax, eax],                     Right [0x89, 0xc0]),
+  (mov [ref1 EAX, eax],                Right [0x8b, 0x00]),
+  (mov [ref1 ESP, eax],                Right [0x8b, 0x04, 0x24]),
+  (mov [ref1 EBP, eax],                Right [0x8b, 0x45, 0x00]),
+  (mov [ref8 4(ECX), eax],             Right [0x8b, 0x41, 0x04]),
+  (mov [ref8 4(EBP), eax],             Right [0x8b, 0x45, 0x04]),
+  (mov [ref8 4(ESP), eax],             Right [0x8b, 0x44, 0x24, 0x04]),
+  (mov [moff 0xdeadbeef, eax],         Right [0xa1, 0xef, 0xbe, 0xad, 0xde]),
+  (mov [moff 0xdeadbeef, ecx],         Right [0x8b, 0x0d, 0xef, 0xbe, 0xad, 0xde])
   ]
 
-main = do
-    let errors = testCodegen movTests
-    putStrLn $ printf "%d tests passed" (length movTests - length errors)
+push = Op [] "push"
+pushTests = [
+  (push [eax],                         Right [0x50]),
+  (push [esp],                         Right [0x54])
+  ]
+
+runTests tests = do
+    let errors = testCodegen tests
+    putStrLn $ printf "%d tests passed" (length tests - length errors)
     putStrLn ""
     forM_ errors $ \(op, expected, got) -> do
         putStrLn $ "For: " ++ pretty op
         putStrLn $ "  expected: " ++ show expected
         putStrLn $ "       got: " ++ show got
         putStrLn ""
+
+main = do
+    runTests movTests
+    runTests pushTests
