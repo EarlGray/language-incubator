313fc286d5c8544fc4ad03671e34b69cb1c56f8a
Dmytro S <me@dmytrish.net>
2020-12-18 01:54:52 +0000

    slothjs: index.js falls back to stdin

--- js/slothjs/index.js
+++ js/slothjs/index.js
@@ -1,11 +1,7 @@
 const fs = require('fs');
 const esprima = require('esprima');
 
-let filename = process.argv[2];
-if (!filename) {
-    console.error('A file name is expected');
-    process.exit();
-}
+let filename = process.argv[2] || '/dev/stdin';
 
 let contents = fs.readFileSync(filename, 'utf8');
 let ast = esprima.parse(contents);
