8773995ab3fbe1c8110da4fb3769a7f957d1f3d5
Dmytro S <me@dmytrish.net>
2020-12-18 01:55:18 +0000

    slothjs: initial support of binary operators, variables

--- js/slothjs/src/main.rs
+++ js/slothjs/src/main.rs
@@ -1,69 +1,99 @@
 use std::io;
 use std::fmt::Debug;
+use std::collections::HashMap;
 
 use serde_json::Value as JSON;
+use serde_json::json;
 
 
 #[derive(Debug)]
-enum Error<'a> {
+enum ParseError<'a> {
     ShouldBeString{ value: &'a JSON },
-    ShouldBeObject{ value: &'a JSON },
     ShouldBeArray{ value: &'a JSON },
-    ObjectWithout{ value: &'a JSON, attr: &'static str },
+    //ShouldBeObject{ value: &'a JSON },
+    ObjectWithout{ attr: &'static str, value: &'a JSON},
     UnexpectedValue{ value: &'a JSON, want: &'static str },
-    UnknownType{ value: &'a JSON, ty: &'a str},
+    UnknownType{ ty: &'a str, value: &'a JSON },
 }
 
-trait Interptetable {
-    fn interpret(&self, state: &mut RuntimeState) -> Result<Box<JSON>, Error>;
+#[derive(Debug)]
+enum Exception {
+    ReferenceError(String),
 }
 
-trait ASTNode: Interptetable {
+#[derive(Debug, Clone)]
+struct JSValue(JSON);
+
+/*
+ *  JSON helpers
+ */
+fn json_get<'a>(json: &'a JSON, property: &'static str) -> Result<&'a JSON, ParseError<'a>> {
+    json.get(property)
+        .ok_or(ParseError::ObjectWithout{ attr: property, value: json })
+}
+
+fn json_get_str<'a>(
+    json: &'a JSON,
+    property: &'static str
+) -> Result<&'a str, ParseError<'a>> {
+    let jstr = json_get(json, property)?;
+    jstr.as_str().ok_or(ParseError::ShouldBeString{ value: jstr })
+}
+
+fn json_get_array<'a>(
+    json: &'a JSON,
+    property: &'static str
+) -> Result<&'a Vec<JSON>, ParseError<'a>> {
+    let jarray = json_get(json, property)?;
+    jarray.as_array().ok_or(ParseError::ShouldBeArray{ value: jarray })
+}
+
+fn json_expect_str<'a>(
+    json: &'a JSON,
+    property: &'static str,
+    value: &'static str
+) -> Result<(), ParseError<'a>> {
+    let jstr = json_get(json, property)?;
+    let got = jstr.as_str().ok_or(ParseError::ShouldBeString{ value: jstr })?;
+    if got == value {
+        Ok(())
+    } else {
+        Err(ParseError::UnexpectedValue{ value: jstr, want: value })
+    }
+}
+
+
+trait Interpretable {
+    fn interpret(&self, state: &mut RuntimeState) -> Result<JSValue, Exception>;
+}
+
+trait ASTNode: Interpretable {
     const TYPE_ID: &'static str;
 
-    fn from_json(value: &JSON) -> Result<Box<Self>, Error>;
-
+    fn from_json(value: &JSON) -> Result<Box<Self>, ParseError>;
 }
 
-trait Statement: ASTNode {}
-
 struct Program {
-    body: Vec<Box<dyn Interptetable>>,
+    body: Vec<Box<dyn Interpretable>>,
 }
 
 impl Program {
-    fn from_json(json: &JSON) -> Result<Box<Self>, Error> {
-        let object = json.as_object()
-            .ok_or(Error::ShouldBeObject{ value: json })?;
-
-        // check type
-        let ty = object.get("type")
-            .ok_or(Error::ObjectWithout{ value: json, attr: "type" })?;
-        if ty.as_str() != Some("Program") {
-            return Err(Error::UnexpectedValue{
-                value: &ty,
-                want: "Program"
-            });
-        };
+    fn from_json(json: &JSON) -> Result<Box<Self>, ParseError> {
+        json_expect_str(json, "type", "Program")?;
 
         // parse body
-        let jbody = json.get("body")
-            .ok_or(Error::ObjectWithout{ value: json, attr: "body"})?;
-        let jbody = jbody.as_array()
-            .ok_or(Error::ShouldBeArray{ value: jbody })?;
+        let jbody = json_get_array(json, "body")?;
 
-        let mut body = Vec::<Box<dyn Interptetable>>::new();
+        let mut body = Vec::new();
         for jstmt in jbody.iter() {
-            let jty = jstmt.get("type")
-                .ok_or(Error::ObjectWithout{ value: jstmt, attr: "type" })?;
-            let ty = jty.as_str()
-                .ok_or(Error::ShouldBeString{ value: jty })?;
-
-            let stmt = match ty {
-                "ExpressionStatement" =>
+            let ty = json_get_str(jstmt, "type")?;
+            let stmt: Box<dyn Interpretable> = match ty {
+                ExpressionStatement::TYPE_ID =>
                     ExpressionStatement::from_json(jstmt)?,
+                VariableDeclaration::TYPE_ID =>
+                    VariableDeclaration::from_json(jstmt)?,
                 _ =>
-                    return Err(Error::UnknownType{ value: jstmt, ty: ty})
+                    return Err(ParseError::UnknownType{ value: jstmt, ty})
             };
             body.push(stmt);
         }
@@ -71,9 +101,11 @@ impl Program {
         Ok(Box::new(Program { body }))
     }
 
-    fn interpret(&self) -> Result<Box<JSON>, Error> {
-        let mut runtime = RuntimeState{};
-        let mut result = Box::new(JSON::Null);
+    fn interpret(&self) -> Result<JSValue, Exception> {
+        let mut runtime = RuntimeState{
+            variables: HashMap::new(),
+        };
+        let mut result = JSValue(JSON::Null);
         for stmt in self.body.iter() {
             result = stmt.interpret(&mut runtime)?;
         }
@@ -83,8 +115,89 @@ impl Program {
 
 // ==============================================
 enum Expr {
-    Literal(JSON),
+    Literal(JSValue),
+    Identifier(String),
+    BinaryOp(Box<Expr>, BinOp, Box<Expr>),
+    Call(Box<Expr>, Vec<Box<Expr>>),
 }
+
+enum BinOp { Add }
+
+impl Expr {
+    fn from_json(jexpr: &JSON) -> Result<Expr, ParseError> {
+        let jexpr_ty = json_get_str(jexpr, "type")?;
+
+        let expr = match jexpr_ty {
+            "BinaryExpression" => {
+                let jleft = json_get(jexpr, "left")?;
+                let left = Expr::from_json(&jleft)?;
+
+                let jright = json_get(jexpr, "right")?;
+                let right = Expr::from_json(&jright)?;
+
+                let opstr = json_get_str(jexpr, "operator")?;
+                let op = match opstr {
+                    "+" => BinOp::Add,
+                    _ => return Err(ParseError::UnexpectedValue{
+                        value: jexpr.get("operator").unwrap(),
+                        want: "+|"
+                    }),
+                };
+                Expr::BinaryOp(Box::new(left), op, Box::new(right))
+            }
+            "CallExpression" => {
+                let jcallee = json_get(jexpr, "callee")?;
+                let callee = Expr::from_json(jcallee)?;
+
+                let jarguments = json_get_array(jexpr, "arguments")?;
+                let arguments: Result<Vec<Box<Expr>>, ParseError> = jarguments.iter()
+                    .map(|j| Expr::from_json(j)
+                    .map(|e| Box::new(e)))
+                    .collect();
+
+                Expr::Call(Box::new(callee), arguments?)
+            }
+            "Identifier" => {
+                let name = json_get_str(jexpr, "name")?;
+                Expr::Identifier(name.to_string())
+            }
+            "Literal" => {
+                let jval = json_get(jexpr, "value")?;
+                Expr::Literal(JSValue(jval.clone().take()))
+            }
+            _ =>
+                return Err(ParseError::UnknownType{ value: jexpr, ty: jexpr_ty}),
+        };
+        Ok(expr)
+    }
+
+    fn interpret(&self, state: &mut RuntimeState) -> Result<JSValue, Exception> {
+        match self {
+            Expr::Literal(val) =>
+                Ok(val.clone()),
+            Expr::Identifier(name) =>
+                state.lookup_identifier(&name),
+            Expr::BinaryOp(lexpr, op, rexpr) => {
+                let lval = lexpr.interpret(state)?;
+                let rval = rexpr.interpret(state)?;
+                match op {
+                    BinOp::Add => {
+                        if let Some(lnum) = lval.0.as_f64() {
+                            if let Some(rnum) = rval.0.as_f64() {
+                                return Ok(JSValue(json!(lnum + rnum)));
+                            }
+                        }
+                        panic!("TODO: adding non-numbers");
+                    }
+                }
+            }
+            Expr::Call(_callee, _arguments) => {
+                panic!("TODO: Expr::Call");
+            }
+        }
+    }
+}
+
 
 struct ExpressionStatement {
     expression: Expr,
@@ -93,59 +206,120 @@ struct ExpressionStatement {
 impl ASTNode for ExpressionStatement {
     const TYPE_ID: &'static str = "ExpressionStatement";
 
-    fn from_json(value: &JSON) -> Result<Box<Self>, Error> {
-        let ty = value.get("type")
-            .ok_or(Error::ObjectWithout{ value, attr: "type" })?;
-        if ty.as_str() != Some(Self::TYPE_ID) {
-            return Err(Error::UnexpectedValue{
-                value: &ty,
-                want: Self::TYPE_ID
-            });
-        }
+    fn from_json(value: &JSON) -> Result<Box<Self>, ParseError> {
+        json_expect_str(value, "type", Self::TYPE_ID)?;
 
-        let jexpr = value.get("expression")
-            .ok_or(Error::ObjectWithout{ value, attr: "expression"})?;
-        let jexpr_ty = jexpr.get("type")
-            .ok_or(Error::ObjectWithout{ value, attr: "type"})?;
-        let jexpr_ty = jexpr_ty.as_str()
-            .ok_or(Error::ShouldBeString{ value })?;
-
-        let expression = match jexpr_ty {
-            "Literal" => {
-                let jval = jexpr.get("value")
-                    .ok_or(Error::ObjectWithout{ value: jexpr, attr: "value" })?;
-                Expr::Literal(jval.clone().take())
-            }
-            _ =>
-                return Err(Error::UnknownType{ value: jexpr, ty: jexpr_ty}),
-        };
-
+        let jexpr = json_get(value, "expression")?;
+        let expression = Expr::from_json(jexpr)?;
         Ok(Box::new(ExpressionStatement { expression }))
     }
 }
 
-impl Interptetable for ExpressionStatement {
-    fn interpret(&self, _state: &mut RuntimeState) -> Result<Box<JSON>, Error> {
-        match &self.expression {
-            Expr::Literal(val) => Ok(Box::new(val.clone())),
+impl Interpretable for ExpressionStatement {
+    fn interpret(&self, state: &mut RuntimeState) -> Result<JSValue, Exception> {
+        self.expression.interpret(state)
+    }
+}
+
+struct VariableDeclarator {
+    name: String,
+    init: Option<Box<Expr>>,
+}
+
+enum DeclarationKind { Var, Let, Const }
+
+struct VariableDeclaration {
+    kind: DeclarationKind,
+    declarations: Vec<VariableDeclarator>,
+}
+
+impl ASTNode for VariableDeclaration {
+    const TYPE_ID: &'static str = "VariableDeclaration";
+
+    fn from_json(value: &JSON) -> Result<Box<Self>, ParseError> {
+        json_expect_str(value, "type", Self::TYPE_ID)?;
+
+        let kind = match json_get_str(value, "kind")? {
+            "const" => DeclarationKind::Const,
+            "let" => DeclarationKind::Let,
+            "var" => DeclarationKind::Var,
+            _ => return Err(ParseError::UnexpectedValue{
+                value: value.get("kind").unwrap(),
+                want: "const|let|var",
+            }),
+        };
+        let jdeclarations = json_get_array(value, "declarations")?;
+
+        let mut declarations = vec![];
+        for decl in jdeclarations.iter() {
+            json_expect_str(decl, "type", "VariableDeclarator")?;
+
+            let jid = json_get(decl, "id")?;
+            json_expect_str(jid, "type", "Identifier")?;
+            let name = json_get_str(jid, "name")?.to_string();
+
+            let jinit = json_get(decl, "init")?;
+            let init = match jinit {
+                JSON::Null => None,
+                _ => {
+                    let expr = Expr::from_json(jinit)?;
+                    Some(Box::new(expr))
+                }
+            };
+
+            declarations.push(VariableDeclarator{ name, init });
+        }
+        Ok(Box::new(VariableDeclaration{ kind, declarations }))
+    }
+}
+
+impl Interpretable for VariableDeclaration {
+    fn interpret(&self, state: &mut RuntimeState) -> Result<JSValue, Exception> {
+        for decl in self.declarations.iter() {
+            state.declare_identifier(decl.name.clone())?;
+            if let Some(init) = &decl.init {
+                let value = init.interpret(state)?;
+                state.set_identifier(&decl.name, value)?;
+            }
+        }
+        Ok(JSValue(JSON::Null))
+    }
+}
+
+// ==============================================
+
+struct RuntimeState {
+    variables: HashMap<String, JSValue>,
+}
+
+impl RuntimeState {
+    fn declare_identifier(&mut self, name: String) -> Result<(), Exception> {
+        let undefined = JSValue(JSON::Null);    // not quite, but works for now
+        self.variables.entry(name).or_insert(undefined);
+        Ok(())
+    }
+
+    fn set_identifier(&mut self, name: &str, value: JSValue) -> Result<(), Exception> {
+        let entry = self.variables.get_mut(name)
+            .ok_or(Exception::ReferenceError(name.to_string()))?;
+        *entry = value;
+        Ok(())
+    }
+
+    fn lookup_identifier(&self, name: &str) -> Result<JSValue, Exception> {
+        match self.variables.get(name) {
+            None => Err(Exception::ReferenceError(name.to_string())),
+            Some(value) => Ok(value.clone()),
         }
     }
 }
 
 // ==============================================
 
-struct RuntimeState {}
-
-
-// ==============================================
-
-fn die<T, E: Debug>(msg: &str, exitcode: i32) -> impl FnOnce(E) -> T
-{
+fn die<E: Debug>(msg: &str, err: E, exitcode: i32) -> ! {
     let msg = msg.to_string();
-    move |e: E| {
-        eprintln!("{}: {:?}", msg, e);
-        std::process::exit(exitcode)
-    }
+    eprintln!("{}: {:?}", msg, err);
+    std::process::exit(exitcode);
 }
 
 fn main() {
@@ -155,17 +329,18 @@ fn main() {
     // thanks to https://stackoverflow.com/a/55797976/621719
     let deserializer= serde_json::Deserializer::from_reader(stdin);
     for json in deserializer.into_iter::<JSON>() {
-        let json = json.unwrap_or_else(die("JSON error", 1));
+        let json = json
+            .unwrap_or_else(|e| die("JSON error", e, 1));
 
         let ast = Program::from_json(&json)
-            .unwrap_or_else(die("Parse error", 2));
+            .unwrap_or_else(|e| die("Parse error", e, 2));
 
         let result = ast.interpret()
-            .unwrap_or_else(die("Interpretation error", 3));
+            .unwrap_or_else(|e| die("Interpretation error", e, 3));
 
         // TODO: JSON output, optionally
-        let output = serde_json::to_string(&result)
-            .unwrap_or_else(die("Result serialization error", 4));
+        let output = serde_json::to_string(&result.0)
+            .unwrap_or_else(|e| die("Result serialization error", e, 4));
 
         println!("{}", output);
     }
