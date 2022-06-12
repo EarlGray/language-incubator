5150e649a64159ed68783a4a0511ae1ff02a672d
Dmytro S <me@dmytrish.net>
2022-06-12 16:41:57 +0100

    slothjs-wasm: use JsValue::from_serde()

--- js/slothjs/wasm/src/lib.rs
+++ js/slothjs/wasm/src/lib.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use slothjs::{
     Interpretable,
     JSON,
@@ -22,23 +23,21 @@ pub fn run() {
     console_error_panic_hook::set_once();
 }
 
+fn jserror<E: fmt::Debug>(e: E) -> JsValue {
+    JsValue::from(format!("{:?}", e))
+}
+
 #[wasm_bindgen]
 pub fn interpret(json_ast: &str) -> Result<JsValue, JsValue> {
-    let json = serde_json::from_str::<JSON>(json_ast)
-        .map_err(|e| JsValue::from(format!("{}", e)))?;
+    let json: JSON = serde_json::from_str(json_ast)
+        .map_err(jserror)?;
     let program = Program::parse_from(&json)
-        .map_err(|e| JsValue::from(format!("{:?}", e)))?;
+        .map_err(jserror)?;
 
     let mut heap = Heap::new();
-    let result = program.interpret(&mut heap)
-        .map_err(|e| JsValue::from(format!("{:?}", e)))?
-        .to_value(&heap)
-        .map_err(|e| JsValue::from(format!("{:?}", e)))?;
-    Ok(match result {
-        slothjs::JSValue::Undefined => JsValue::NULL,
-        slothjs::JSValue::Number(n) => JsValue::from(n),
-        slothjs::JSValue::Bool(b) => JsValue::from(b),
-        slothjs::JSValue::String(s) => JsValue::from(s),
-        _ => todo!(),
-    })
+    let result = program.interpret(&mut heap).map_err(jserror)?
+        .to_value(&heap).map_err(jserror)?
+        // TODO: implement serde::Serializer directly for JSValue?
+        .to_json(&heap).map_err(jserror)?;
+    JsValue::from_serde(&result).map_err(jserror)
 }
