109cefcdd2f212dc7016c4152fbb01ff8aabccca
Dmytro S <me@dmytrish.net>
2023-03-10 00:25:56 +0100

    ffi/py/c : the minimal working example

---
+++ ffi/py/c/spam.c
@@ -0,0 +1,40 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+static PyObject *
+spam_system(PyObject *self, PyObject *args)
+{
+    const char *command;
+    int sts;
+
+    if (!PyArg_ParseTuple(args, "s", &command))
+        return NULL;
+    sts = system(command);
+    return PyLong_FromLong(sts);
+}
+
+static PyObject *SpamError;
+
+static const char
+spam_doc[] = "See https://docs.python.org/3/extending/extending.html";
+
+static PyMethodDef SpamMethods[] = {
+    {"system",  spam_system, METH_VARARGS,
+     "Execute a shell command."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+static struct PyModuleDef spammodule = {
+    PyModuleDef_HEAD_INIT,
+    "spam",   /* name of module */
+    spam_doc, /* module documentation, may be NULL */
+    -1,       /* size of per-interpreter state of the module,
+                 or -1 if the module keeps state in global variables. */
+    SpamMethods
+};
+
+PyMODINIT_FUNC
+PyInit_spam(void)
+{
+    return PyModule_Create(&spammodule);
+}
