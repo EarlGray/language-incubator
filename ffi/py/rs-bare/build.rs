0d7aee9a86bbe00d825e943e2a4a715152f9da7a
Dmytro S <me@dmytrish.net>
2023-03-11 00:30:00 +0100

    ffi/py/rs-bare/ : some tweaks

--- ffi/py/rs-bare/build.rs
+++ ffi/py/rs-bare/build.rs
@@ -1,11 +1,15 @@
 use std::path::PathBuf;
 use std::process::Command;
 
+const OUTPUT: &str = "src/sys/mod.rs";
+
 const PYTHON_CONFIG: &str = "python3-config";
 const PYTHON_H: &str = "Python.h";
 const CLANG: &str = "clang";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=build.rs");
+
     Command::new(CLANG).arg("--version").status()
         .map_err(|_| format!("{CLANG} is required by bindgen"))?;
 
@@ -16,16 +20,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let path = PathBuf::from_iter(&[dir, PYTHON_H]);
         path.exists().then_some(path)
     }) else {
-        return Err(format!("{} not found in {}", PYTHON_H, python_includes).into());
+        return Err(format!("{PYTHON_H} not found in {python_includes}").into());
     };
+    let python_header = python_header.to_str()
+        .ok_or_else(|| format!("{python_header:?} is not UTF-8"))?;
+    println!("cargo:rerun-if-changed={}", python_header);
 
     let python_cflags = Command::new(PYTHON_CONFIG).arg("--cflags").output()?;
     let python_cflags = String::from_utf8(python_cflags.stdout)?;
 
     let bindings = bindgen::builder()
         .clang_args(python_cflags.split_ascii_whitespace())
-        .header(python_header.to_str().unwrap())
+        .header(python_header)
         .generate()?;
-    bindings.write_to_file("src/sys/mod.rs")?;
+    bindings.write_to_file(OUTPUT)?;
+
+    // TODO: output linker args, https://doc.rust-lang.org/cargo/reference/build-scripts.html
     Ok(())
 }
